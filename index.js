238205c10f59331d6d73d6659655d4b3dfd42365
Minku Singh <mailminkusingh@gmail.com>
2022-04-22 12:55:14 +0530

    configure: dotenv

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require("dotenv").config()
 const express = require("express");
 const request = require("request-promise");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const generateScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+const apiKey = process.env.API_KEY;
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
 
 app.use(express.json());
 
@@ -18,7 +20,7 @@ app.get("/products/:productId", async (req, res) => {
     const {api_key} = req.query;
 
     try{
-        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`)
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`)
         res.json(JSON.parse(response));
     }catch(error){
         res.json({error: error.message})
@@ -28,10 +30,9 @@ app.get("/products/:productId", async (req, res) => {
 // get product reviews 
 app.get("/products/:productId/reviews", async (req, res) => {
     const {productId} = req.params;
-    const {api_key} = req.query;
 
     try{
-        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productId}`)
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`)
         res.json(JSON.parse(response));
     }catch(error){
         res.json({error: error.message})
@@ -44,7 +45,7 @@ app.get("/products/:productId/offers", async (req, res) => {
     const {api_key} = req.query;
 
     try{
-        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`)
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`)
         res.json(JSON.parse(response));
     }catch(error){
         res.json({error: error.message})
@@ -54,10 +55,9 @@ app.get("/products/:productId/offers", async (req, res) => {
 // get search results 
 app.get("/search/:searchQuery", async (req, res) => {
     const {searchQuery} = req.params;
-    const {api_key} = req.query;
 
     try{
-        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`)
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`)
         res.json(JSON.parse(response));
     }catch(error){
         res.json({error: error.message})
